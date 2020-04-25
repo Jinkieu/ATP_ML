1bf25b2405214c1736e6c6832b32a60313a37230
jinkieu <quachjulien@gmail.com>
2020-04-26 00:51:05 +0200

    train test sets, fit and score

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import sqlite3
 from atp_db import save_df
 from atp_db import get_data
 import pandas as pd
+import numpy as np
+import random
+from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
 
 cnx = sqlite3.connect('atp.db')
 cursor = cnx.cursor()
@@ -17,12 +23,13 @@ def most_win():
 
 
 def calculate_winrate(player, idx):
-    new_df = df[['winner_name', 'loser_name']].iloc[0:idx].loc[(df['winner_name'] == player) | (df['loser_name'] == player)]
+    new_df = df[['winner_name', 'loser_name']].iloc[0:idx].loc[
+        (df['winner_name'] == player) | (df['loser_name'] == player)]
     total_game = len(new_df)
     total_win = len(new_df.loc[new_df['winner_name'] == player])
     if total_game == 0:
         return 0
-    return total_win/total_game
+    return total_win / total_game
 
 
 def player_winrate_before_match():
@@ -71,9 +78,154 @@ def analyse_2018(df_2018):
     # However this analysis can be biased because there are way more right-handed players than southpaws
 
 
+def get_random_player(winner, loser):
+    tab = [winner, loser]
+    return random.choice(tab)
+
+
+def define_game_won(player, winner):
+    if player == winner:
+        return 1
+    else:
+        return 0
+
+
+def get_player_rank(player, winner, l_rank, w_rank):
+    if player == winner:
+        return w_rank
+    else:
+        return l_rank
+
+
+def get_opponent_name(player, winner, loser):
+    if player == winner:
+        return loser
+    else:
+        return winner
+
+
+def get_player_age(player, winner, l_age, w_age):
+    if player == winner:
+        return w_age
+    else:
+        return l_age
+
+
+def get_player_hand(player, winner, l_hand, w_hand):
+    if player == winner:
+        return w_hand
+    else:
+        return l_hand
+
+
+def get_player_winrate(player, winner, l_wr, w_wr):
+    if player == winner:
+        return w_wr
+    else:
+        return l_wr
+
+
+def get_player_height(player, winner, l_ht, w_ht):
+    if player == winner:
+        return w_ht
+    else:
+        return l_ht
+
+
+def create_new_df(a):
+    data = a.iloc[50000:99144]
+    # creation of a new column : Player (random between Winner/Loser)
+    data['player'] = data.apply(
+        lambda row: get_random_player(row['winner_name'], row['loser_name']), axis=1
+    )
+    # player_result = 0 if row['player'] didn't won, player_result = 1 if row['player'] won)
+    data['player_result'] = data.apply(
+        lambda row: define_game_won(row['player'], row['winner_name']), axis=1
+    )
+    data['player_rank'] = data.apply(
+        lambda row: get_player_rank(row['player'], row['winner_name'], row['loser_rank'], row['winner_rank']), axis=1
+    )
+    data['player_age'] = data.apply(
+        lambda row: get_player_age(row['player'], row['winner_name'], row['loser_age'], row['winner_age']), axis=1
+    )
+    data['player_hand'] = data.apply(
+        lambda row: get_player_hand(row['player'], row['winner_name'], row['loser_hand'], row['winner_hand']), axis=1
+    )
+    data['player_winrate'] = data.apply(
+        lambda row: get_player_winrate(row['player'], row['winner_name'], row['loser_winrate'], row['winner_winrate']),
+        axis=1
+    )
+    data['player_height'] = data.apply(
+        lambda row: get_player_height(row['player'], row['winner_name'], row['loser_ht'], row['winner_ht']), axis=1
+    )
+    # Create columns for opponent's stats
+    data['opponent'] = data.apply(
+        lambda row: get_opponent_name(row['player'], row['winner_name'], row['loser_name']), axis=1
+    )
+    data['opponent_rank'] = data.apply(
+        lambda row: get_player_rank(row['opponent'], row['winner_name'], row['loser_rank'], row['winner_rank']), axis=1
+    )
+    data['opponent_age'] = data.apply(
+        lambda row: get_player_age(row['opponent'], row['winner_name'], row['loser_age'], row['winner_age']), axis=1
+    )
+    data['opponent_hand'] = data.apply(
+        lambda row: get_player_hand(row['opponent'], row['winner_name'], row['loser_hand'], row['winner_hand']), axis=1
+    )
+    data['opponent_winrate'] = data.apply(
+        lambda row: get_player_winrate(row['opponent'], row['winner_name'], row['loser_winrate'], row['winner_winrate'])
+        , axis=1
+    )
+    data['opponent_height'] = data.apply(
+        lambda row: get_player_height(row['opponent'], row['winner_name'], row['loser_ht'], row['winner_ht']), axis=1
+    )
+    data_final = data[['surface', 'date', 'tourney_level',
+                       'opponent_rank', 'opponent_age', 'opponent_hand', 'opponent_winrate',
+                       'opponent_height'
+                       , 'player_rank', 'player_age', 'player_hand', 'player_winrate', 'player_height'
+                       , 'player_result'
+                       ]]
+    return data_final
+
 def main():
-    df_2018 = get_df_2018(df_db)
-    analyse_2018(df_2018)
+
+    # 1.
+    # df_2018 = get_df_2018(df_db)
+    # analyse_2018(df_2018)
+
+    # 2.
+    data = create_new_df(df_db)
+
+    # handle dummies
+    cat_vars = ['surface', 'tourney_level', 'opponent_hand', 'player_hand']
+    for var in cat_vars:
+        cat_list = 'var' + '_' + var
+        cat_list = pd.get_dummies(data[var], prefix=var)
+        data1 = data.join(cat_list)
+        data = data1
+    data_vars = data.columns.values.tolist()
+    to_keep = [i for i in data_vars if i not in cat_vars]
+    data_final = data[to_keep]
+
+    # train and test sets
+    idx_2018_min = 98115
+    id_2018_max = 99144
+    x_axis = data_final.loc[:, data_final.columns != 'player_result']
+    y_axis = data_final.loc[:, data_final.columns == 'player_result']
+
+    x_test = x_axis.iloc[idx_2018_min:id_2018_max]
+
+    y_test = y_axis.iloc[idx_2018_min:id_2018_max]
+
+    x_train = x_axis.iloc[:idx_2018_min]
+
+    y_train = y_axis.iloc[:idx_2018_min]
+
+    model = LogisticRegression()
+
+    model.fit(x_train, y_train)
+
+    print("here's the score")
+    model.score(x_test, y_test)
 
 
 main()
